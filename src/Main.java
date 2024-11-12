e968d9d1289bdfdf96f03f60f9622382ea9ecf5c
LENOVO <sworupdhakal88@gmail.com>
2024-11-12 14:04:43 +0545

    j

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import models.Book;
+import models.BookOrder;
+import models.PhysicalBook;
+import models.Ebook;
+import models.Genre;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Genre g1 = new Genre("Fiction", "Best Fiction");
+        Genre g2 = new Genre("Fiction", "Good Fiction");
+        System.out.println(g1.getGenreName());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        g1.setFiction("This is my fiction");
+        System.out.println(g1.getFiction());
+
+        Book b1 = new Book("Mangena","Ram","Sahitya","Serial",2012,g1);
+
+        BookOrder order1 = new BookOrder(1,2065);
+
+        Ebook book1 = new Ebook("Mangena","Ram","Sahitya","Serial",2065,g1,21.65);
+
+        PhysicalBook book2 = new PhysicalBook("Mangena","Ram","Sahitya","Serial",2012,g1,"Poor");
+
+        System.out.println("Book 1"+ b1.getPublishedYear());
+        System.out.println("Book 2"+ book1.getBookname());
+
     }
 }
